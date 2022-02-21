d4440e0079ae8d3a4a444e9f2deff6cd81931329
rimthekid <cppoxking@gmail.com>
2022-02-21 16:17:01 +0330

    init

---
+++ test.py
@@ -0,0 +1,39 @@
+import asyncio
+from telethon import TelegramClient,events
+k=5
+
+level = 0
+netwin=30
+bet=30
+loss=0
+# Remember to use your own values from my.telegram.org!
+api_id = 17501677
+api_hash = 'ee61461e144e8a59824cf5adb6eee60d'
+client = TelegramClient('an', api_id, api_hash)
+
+
+
+@client.on(events.NewMessage(from_users="lntxbot",pattern="(?i)You've lost.+"))
+async def handler(event):
+    # Respond whenever someone says "Hello" and something else
+    global loss,level,bet,netwin
+    level +=1
+    loss += bet + 10 
+    netwin += k
+    bet=loss+netwin
+    await event.reply('level: '+str(level)+'\n'+'total loss: '+str(loss)+'\n'+'net win: '+str(netwin)+'\n'+'bet: '+str(bet))
+    await client.send_message('lncasino','/coinflip '+str(bet))
+    
+@client.on(events.NewMessage(from_users="lntxbot",pattern="(?i)You're the winner.+"))
+async def handler(event):
+    # Respond whenever someone says "Hello" and something else
+    global loss,level,bet,netwin
+    level = 0
+    netwin=30
+    bet=30
+    loss=0
+    await client.send_message('lncasino','/coinflip '+str(bet))
+
+
+client.start()
+client.run_until_disconnected()
